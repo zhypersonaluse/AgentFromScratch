d5b421a2b276cb62692d0c1179c068c97a409f9e
vboxuser <zhypersonaluse@gmail.com>
2026-06-15 02:02:26 +0000

    tools

---
+++ react.py
@@ -0,0 +1,22 @@
+from dotenv import load_dotenv
+from langchain_core.tools import tool
+from langchain_openai import ChatOpenAI
+from langchain_tavily import TavilySearch
+
+load_dotenv()
+
+@tool
+def triple(num: float) -> float:
+    """
+    :param num: a number to triple
+    :return: the number tripled ->  multiplied by 3
+    """
+    return 3 * float(num)
+
+
+tools = [TavilySearch(max_results=1), triple]
+
+
+llm = ChatOpenAI(model="gpt-4o", temperature=0).bind_tools(tools)
+
+
